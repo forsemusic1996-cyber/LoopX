93ddda6e4e46655f5ce4c55777e8dbdee2bcbda6
forsemusic1996-cyber <forsemusic1996@gmail.com>
2026-09-18 12:55:05 +0300

    Add resizable waveform editor
    Refactor MiniSamplerAudioProcessorEditor to include file drag-and-drop support, change listener, and timer functionality.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -2,21 +2,38 @@
 
 #include "PluginProcessor.h"
 
-class MiniSamplerAudioProcessorEditor final : public juce::AudioProcessorEditor
+class MiniSamplerAudioProcessorEditor final : public juce::AudioProcessorEditor,
+                                              private juce::FileDragAndDropTarget,
+                                              private juce::ChangeListener,
+                                              private juce::Timer
 {
 public:
     explicit MiniSamplerAudioProcessorEditor(MiniSamplerAudioProcessor&);
-    ~MiniSamplerAudioProcessorEditor() override = default;
+    ~MiniSamplerAudioProcessorEditor() override;
 
     void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
+    bool isInterestedInFileDrag(const juce::StringArray& files) override;
+    void filesDropped(const juce::StringArray& files, int x, int y) override;
+    void changeListenerCallback(juce::ChangeBroadcaster*) override;
+    void timerCallback() override;
+    void loadSampleFile(const juce::File& file);
+    void drawTempoGrid(juce::Graphics&, juce::Rectangle<float>) const;
+
     MiniSamplerAudioProcessor& processor;
     juce::TextButton loadButton { "Load sample..." };
     juce::Label sampleLabel;
     juce::MidiKeyboardComponent keyboard;
     std::unique_ptr<juce::FileChooser> fileChooser;
+    juce::AudioFormatManager thumbnailFormatManager;
+    juce::AudioThumbnailCache thumbnailCache { 5 };
+    juce::AudioThumbnail thumbnail { 512, thumbnailFormatManager, thumbnailCache };
+    juce::Rectangle<int> waveformBounds;
+    double displayedTempo { 120.0 };
+    int displayedNumerator { 4 };
+    int displayedDenominator { 4 };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MiniSamplerAudioProcessorEditor)
 };
